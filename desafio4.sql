4d0e4e3d4c026f2ec926e9f0ef42fcf8c217613d
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-02-04 21:10:06 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT us.user_name AS usuario,
+IF(
+	DATEDIFF(DATE(MAX(pl.date_played)), CURRENT_DATE()) < -365,
+    'Usuário ativo',
+    'Usuário inativo'
+) AS condicao_usuario
+FROM SpotifyClone.user AS us
+JOIN SpotifyClone.plays AS pl
+ON pl.user_id = us.user_id
+GROUP BY pl.user_id
+ORDER BY us.user_name;
